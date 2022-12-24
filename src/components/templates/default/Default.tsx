54eca5e553ae2dac06799e90304f4989f7934210
Angelo Dias <angelo@foobar.agency>
2022-12-24 14:09:33 +0100

    Add Default template

--- src/components/templates/default/Default.tsx
+++ src/components/templates/default/Default.tsx
@@ -1,6 +1,13 @@
 import React from "react"
 import { FCC } from "types/types"
+import { Header } from "./header/Header"
 
 export const Default: FCC = ({ children }) => {
-  return <>{children}</>
+  return (
+    <div className="h-full text-white bg-black">
+      <Header />
+      {children}
+      <div>footer</div>
+    </div>
+  )
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,16 @@
 import "../styles/global.css"
 import type { AppProps } from "next/app"
+import { Default as DefaultTemplate } from "components/templates/default/Default"
 import { NextIntlProvider } from "next-intl"
 import IntlPolyfills from "scripts/polyfills"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <NextIntlProvider messages={pageProps.messages} timeZone="Etc/Universal">
-        <IntlPolyfills />
+    <NextIntlProvider messages={pageProps.messages} timeZone="Etc/Universal">
+      <IntlPolyfills />
+      <DefaultTemplate>
         <Component {...pageProps} />
-      </NextIntlProvider>
-    </>
+      </DefaultTemplate>
+    </NextIntlProvider>
   )
 }
