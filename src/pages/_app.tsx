6676ce695e1d6c3fe6ae51706e18f7727a3bfefc
Angelo Dias <angelo@foobar.agency>
2022-12-24 15:41:55 +0100

    Add i18n polyfills

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,15 @@
 import "../styles/global.css"
 import type { AppProps } from "next/app"
 import { NextIntlProvider } from "next-intl"
+import IntlPolyfills from "scripts/polyfills"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <NextIntlProvider messages={pageProps.messages}>
-      <Component {...pageProps} />
-    </NextIntlProvider>
+    <>
+      <NextIntlProvider messages={pageProps.messages} timeZone="Etc/Universal">
+        <IntlPolyfills />
+        <Component {...pageProps} />
+      </NextIntlProvider>
+    </>
   )
 }
