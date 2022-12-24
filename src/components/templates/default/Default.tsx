1e4e50d1966db5624ac6b8bfba31db8e22239fbd
Angelo Dias <angelo@foobar.agency>
2022-12-24 14:22:19 +0100

    Add center grow to glue footer to bottom

--- src/components/templates/default/Default.tsx
+++ src/components/templates/default/Default.tsx
@@ -1,13 +1,14 @@
 import React from "react"
 import { FCC } from "types/types"
+import { Footer } from "./footer/Footer"
 import { Header } from "./header/Header"
 
 export const Default: FCC = ({ children }) => {
   return (
-    <div className="h-full text-white bg-black">
+    <div className="flex flex-col h-full text-white bg-black">
       <Header />
-      {children}
-      <div>footer</div>
+      <div className="flex-grow">{children}</div>
+      <Footer />
     </div>
   )
 }
