66d147d11f1592f895a6367a80fd5e53a3ba84f2
Angelo Dias <oiangelodias@gmail.com>
2022-12-25 11:49:24 +0100

    Add eslint rule (#11)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,25 @@
 module.exports = {
   root: true,
-  extends: ["next", "next/core-web-vitals", "prettier"],
+  extends: ["next", "next/core-web-vitals"],
+  plugins: ["unused-imports"],
   rules: {
     "react/react-in-jsx-scope": "off",
+    "no-unused-vars": "off",
+    "unused-imports/no-unused-imports": "error",
+    "unused-imports/no-unused-vars": [
+      "warn",
+      {
+        vars: "all",
+        varsIgnorePattern: "^_",
+        args: "after-used",
+        argsIgnorePattern: "^_",
+      },
+    ],
+    "sort-imports": [
+      "error",
+      {
+        ignoreDeclarationSort: true,
+      },
+    ],
   },
 }
