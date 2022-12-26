15f696a24a2036aee77f2147359d76f43b409255
Angelo Dias <oiangelodias@gmail.com>
2022-12-26 13:43:25 +0100

    [16] Format title (#19)
    This PR adds formatting to the website title — actually a 
    description — and a hidden H1 with the correct website for better 
    SEO.

--- src/components/templates/default/header/SiteTitle.tsx
+++ src/components/templates/default/header/SiteTitle.tsx
@@ -1,5 +1,17 @@
+import { H2 } from "components/typography/typography"
+import { useTranslations } from "next-intl"
 import React from "react"
 
 export const SiteTitle = () => {
-  return <p className="text-center">Let&apos;s test the kink out of you!</p>
+  const t = useTranslations()
+  return (
+    <>
+      {/* Hidden h1 with the actual title */}
+      <h1 className="hidden">{t("common.head.title")}</h1>
+      {/* h2 with the logline */}
+      <H2 className="mx-2 my-8 font-serif text-2xl font-extrabold text-center small-caps text-lightergray">
+        {t("common.title")}
+      </H2>
+    </>
+  )
 }
